7457e229d644c5d6f895fb15862e549d3c382a96
Chromat14 <incrediblesrijan1@gmail.com>
2025-02-23 11:43:59 +0545

    Update search.php

--- search.php
+++ search.php
@@ -30,7 +30,7 @@
           <?php if($admin){
             echo '<a class="nav-link" href="./panel.php">Panel</a>';
           }?>
-          <a class="nav-link" href="#">Help</a>
+          <a class="nav-link" href="./help.php">Help</a>
           <a class="nav-link" href="./faq.php">FAQ</a>
           <a class="nav-link" href="./aboutus.php">About Us</a>
         </div>
@@ -97,7 +97,18 @@
 		</div>
 	</main>
 </div>
-
+<footer class="bg-light py-4 mt-5">
+	<div class="container text-center">
+		<p class="mb-1">&copy; <?php echo date("Y"); ?> Food Ordering System. All rights reserved.</p>
+		<p>Designed & Developed by <a href="#">Sujit Lama & Srijan Bagdas</a></p>
+		<div class="d-flex justify-content-center gap-3 mt-2">
+			<a href="#"><img src="./img/facebook.png" alt="Facebook" width="30"></a>
+			<a href="#"><img src="./img/twitter.png" alt="Twitter" width="30"></a>
+			<a href="#"><img src="./img/instagram.png" alt="Instagram" width="30"></a>
+			
+		</div>
+	</div>
+</footer>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
